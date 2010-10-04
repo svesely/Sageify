d89721c347ddf373a617976da080c71868681b41
Scott Vesely <svesely@gmail.com>
2010-10-04 15:13:34 -0500

    Getting default login redirects back up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,34 +5,17 @@ class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery :secret => "1OlbmzDdcwfiYKxfeAKnDq4WTglo3BpvC3OR581UnzOvkGGnwDozG6HiPP3tZUJN"
   private  
-    # def current_user_session
-    #   return @current_user_session if defined?(@current_user_session)
-    #   @current_user_session = UserSession.find
-    # end
-    # 
-    # def current_user
-    #   return @current_user if defined?(@current_user)
-    #   @current_user = current_user_session && current_user_session.record
-    # end
-    # 
-    # def require_user
-    #   unless current_user
-    #     store_location
-    #     flash[:notice] = "You must be logged in to access this page" unless request.path_info == '/' 
-    #     redirect_to root_url
-    #     return false
-    #   end
-    # end
- 
-    # def require_no_user
-    #   if current_user
-    #     store_location
-    #     flash[:notice] = "You must be logged out to access this page"
-    #     redirect_to "/"
-    #     return false
-    #   end
-    # end
     
+    def after_sign_in_path_for(user)
+      if user.has_role?('Instructional Designer')
+        lessons_url
+      elsif user.has_role?('Learner')
+        learn_index_url
+      else
+        lessons_url
+      end
+    end
+
     def store_location
       session[:return_to] = request.request_uri
     end
@@ -51,17 +34,6 @@ class ApplicationController < ActionController::Base
           format.xml  { render :status => :access_denied }
         end
         return false
-      end
-    end
-
-    def default_for_user(user)
-      #TODO this sucks
-      if user.has_role?('Instructional Designer')
-        root_url
-      elsif user.has_role?('Learner')
-        learn_index_url
-      else
-        root_url
       end
     end
 end
